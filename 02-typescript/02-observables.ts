1376b789a288e97282f2f083b9349c3bd13f572e
alexm91 <alex.moralesc91@gmail.com>
2018-11-12 09:42:09 -0500

    observables

--- 02-typescript/02-observables.ts
+++ 02-typescript/02-observables.ts
@@ -1,5 +1,3 @@
-// npm install rxjs
-
 // 02-observables.ts
 // import { PaqueteUno, Paquete dos} from 'rxjs';
 // import * as rxjs from 'rxjs';
@@ -9,12 +7,41 @@ declare var require: any;
 // declare var module:any;
 
 const rxjs = require('rxjs');
+const map = require('rxjs/operators').map;
+const disctinct = require('rxjs/operators').distinct;
 
-const observableUno$ = rxjs.of(1, 2, 3, 4, 5, 6, 7);
+const observableUno$ = rxjs.of(
+    [1, 2, 3],
+    3,
+    'Hola',
+    3,
+    true,
+    3,
+    {nombre: 'Alex'},
+    new Date(),
+    3,
+);
 
 console.log(observableUno$);
 
 observableUno$
+    .pipe(
+        disctinct(),
+        map(
+            (valor) => {
+                console.log('Valor', valor);
+                return {
+                    data: valor
+                };
+            }
+        )
+    )
+    .pipe(
+
+    )
+    .pipe(
+
+    )
     .subscribe(
         (ok) => {
             console.log('En ok', ok);
@@ -27,3 +54,58 @@ observableUno$
         }
     );
 
+
+const promesita = () => {
+    // @ts-ignore
+    return new Promise(
+        (resolve, reject) => {
+            resolve(':)');
+        }
+    )
+};
+
+
+// @ts-ignore
+const ejecutarCodigoSyncrono = async () => {
+    console.log('Inicio');
+
+    try {
+        const resultadoPromesita = await promesita();
+        console.log(resultadoPromesita);
+    } catch (e) {
+        console.log('Error en promesita', e)
+    }
+    console.log('Fin');
+};
+
+
+class Usuario{
+
+    private async llamar(){
+        await promesita();
+    }
+}
+
+ejecutarCodigoSyncrono();
+
+/*
+const observableDePromesa$ = rxjs.from(promesita());
+observableDePromesa$
+    .pipe(
+        map(
+            (valor) => {
+                return {
+                    data: valor
+                }
+            }
+        )
+    )
+    .subscribe(
+        (objetoFeliz) => {
+            console.log(objetoFeliz);
+        },
+        (error) => {
+            console.log(error);
+        }
+    );
+ */
